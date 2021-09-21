78b720f586a268d4b794b83d59fd9c5095bbe0a6
Christian Silveira <epelococastro@hotmail.com>
2021-09-21 15:15:58 -0300

    ...

---
+++ src/pages/RegisterPage/index.jsx
@@ -0,0 +1,11 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup"
+
+export const RegisterPage = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
